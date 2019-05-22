40d94a5555292551a822f99cf30b9ce4eacfc25a
yzstrive <yzStrive@gmail.com>
2019-05-22 09:56:23 +0800

    fix:config-override

--- config-overrides.js
+++ config-overrides.js
@@ -1,8 +1,6 @@
 /* config-overrides.js */
 
-const rewireReactHotLoader = require('react-app-rewire-hot-loader')
 module.exports = function override(config, env) {
   //do stuff with the webpack config...
-  config = rewireReactHotLoader(config, env)
   return config
 }
