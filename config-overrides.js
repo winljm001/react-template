5ad034e1dc64e7dadf8578d80871ef67249cee0d
yzstrive <yzStrive@gmail.com>
2019-05-26 16:32:15 +0800

    fix: comment link

--- config-overrides.js
+++ config-overrides.js
@@ -1,4 +1,6 @@
 /* config-overrides.js */
+// https://github.com/cdharris/react-app-rewire-hot-loader
+// https://github.com/gaearon/react-hot-loader#getting-started
 const { override, addBabelPlugins } = require('customize-cra')
 // module.exports = function override(config, env) {
 //   //do stuff with the webpack config...
